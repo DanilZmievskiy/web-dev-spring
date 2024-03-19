1e6cf9444eddc07bf5f70efbae6cde97f493c8c8
DanilZmievskiy <danil.zmievskiy@gmail.com>
2024-03-19 13:12:18 +0300

    Added lesson 1 tasks 1, 3-5

--- lesson_1/task/task4.js
+++ lesson_1/task/task4.js
@@ -2,7 +2,21 @@
 // сумма цифр данного трехзначного числа является четным числом. Числа меняются !
 // Пример:
 
-let x = 552
+let x = 552;
+
+let numberToString = String(x).split('').map((x) => parseInt(x));
+
+let sum = 0;
+
+numberToString.forEach(num => {
+	sum += num;
+})
+
+if (sum % 2 == 0) {
+	console.log(true);
+} else {
+	console.log(false);
+}
 
 // Сумма чисел: 5 + 5 + 2 = 12 
 // 12 - четное число (true)
