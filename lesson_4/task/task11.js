e4efa8b83c524233c7fd4f184245e3e816cc28d0
vitaliy <chertkov.vitaliy@gmail.com>
2024-03-23 21:57:56 +0300

    task11.js

--- lesson_4/task/task11.js
+++ lesson_4/task/task11.js
@@ -1,5 +1,7 @@
-//todo: Задан произвольный url необходимо получить его домен.
+//todo: Задан произвольный url необходимо получить router, action и id
 
 Пример:
 var url = "http://www.ozon.ru/context/detail/id/19677670/"
-Домен: www.ozon.ru
+router: context
+action: detail
+id: 19677670
