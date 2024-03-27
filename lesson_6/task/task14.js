8f463165424515b6f079505c2805235dcc04b013
vitaliy <chertkov.vitaliy@gmail.com>
2024-03-27 12:15:21 +0300

    Задание 6

---
+++ lesson_6/task/task14.js
@@ -0,0 +1,3 @@
+// todo: реализовать overlay на tailwind. В статье демонстрируется подход к реализации спинера.
+// https://www.tailwindcsscomponent.com/full-page-overlay-loading-screen#what-is-tailwind-css
+// Запустить код, заменить спинер на сплошную заливку полупрозным фоном.
