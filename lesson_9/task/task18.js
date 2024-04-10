5516e4680f4a6c74c131c10d0615cf9e7408a8d2
vitaliy <chertkov.vitaliy@gmail.com>
2024-04-10 12:52:55 +0300

    Занятие 9

---
+++ lesson_9/task/task18.js
@@ -0,0 +1,15 @@
+// todo: решить Задача 2 см. lesson9.pdf
+// исходный код
+class Car {
+    constructor (f) {
+        this.fuel = f
+        console.log('Маúина создана. Бак заправлен на ' + this.fuel)
+     }
+     reFuel (q) {
+        let diff = (this.fuel + q) - 100
+        this.fuel = (this.fuel + q) <= 100 ? (this.fuel + q) : 100
+        return (this.fuel + q) > 100 ? diff : 0
+     }
+    }
+    let renoLogan = new Car(71)
+
